021852768a7f66a49422fc7b2b84ad2a4037d2c3
Aashish Bhatt <bhattaashish303@gmail.com>
2023-02-01 21:03:51 +0545

    footer

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import Footer from '@/components/Footer/Footer'
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+      <Footer />
+    </div>
+  )
+}
+
+export default about
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
+import Footer from '@/components/Footer/Footer'
 import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
     <>
       <main className={`${styles.main}`}>
-        <h1 className="outline-gradient-text">hello world</h1>
+        <h1 className="outline-gradient-text">HELLO {'    '}WORLD</h1>
         <button className="primary-button">Hello world</button>
+        <div style={{ height: '90vh' }}></div>
+        <Footer />
       </main>
     </>
   )
