752e025ef7855ad43dfcbc5cd383870e99a94cae
Aashish Bhatt <bhattaashish303@gmail.com>
2023-02-01 16:44:19 +0545

    setting up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
+import Head from 'next/head'
+import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Mantra Labs</title>
+        <meta name="description" content="Mantra Labs" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
