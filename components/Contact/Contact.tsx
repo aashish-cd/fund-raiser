ab9400746244f801e0459f74b7d7d42d40977089
Aashish Bhatt <bhattaashish303@gmail.com>
2023-02-02 21:20:01 +0545

    contact text

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -6,7 +6,7 @@ const Contact = () => {
     <div className={styles.contact}>
       <div className={styles.connectPart}>
         <h3>
-          <span>Let&apos;s</span> Connect
+          <span className="outline-gradient-text">Let&apos;s</span> Connect
         </h3>
         <p>Build your business together with MANTRA LABS</p>
       </div>
