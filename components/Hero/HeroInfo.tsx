e399829f26c271989ca33b99f207cfe9eafba1c1
Aashish Bhatt <bhattaashish303@gmail.com>
2023-02-06 12:03:20 +0545

    hero section ongoing

---
+++ components/Hero/HeroInfo.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './HeroInfo.module.scss'
+
+const HeroInfo = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.infoContainer}>
+        <img src="" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default HeroInfo
