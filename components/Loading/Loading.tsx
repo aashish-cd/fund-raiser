fc6bee1ebeda148f89357f26e2b94161a8bf9d2d
Aashish Bhatt <bhattaashish303@gmail.com>
2023-02-08 19:33:45 +0545

    loading page added

---
+++ components/Loading/Loading.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+import React from 'react'
+import styles from './Loading.module.scss'
+const Loading = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.loader}>
+        <h1 className="outline-gradient-text">A</h1>
+        <div className={styles.logoContainer}>
+          <Image
+            src="/mantra.svg"
+            alt="mantra"
+            className={styles.name}
+            height={1213}
+            width={257}
+          />
+          {[1, 2, 3].map((item) => (
+            <Image
+              src="/dot.svg"
+              className={styles.dot}
+              alt="mantra"
+              width={40}
+              height={39}
+              key={item}
+              style={{
+                animationDelay: `${1 + item * 0.3}s`,
+              }}
+            />
+          ))}
+        </div>
+        <h2 className="outline-gradient-text">FOR EVERYTHING</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Loading
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,46 @@
+import { useState } from 'react'
 import Contact from '@/components/Contact/Contact'
 import Footer from '@/components/Footer/Footer'
 import Hero from '@/components/Hero/Hero'
 import HowWeWork from '@/components/HowWeWork/HowWeWork'
+import Loading from '@/components/Loading/Loading'
 import NavBar from '@/components/NavBar/Navbar'
 import OurWork from '@/components/OurWork/OurWork'
 import styles from '../styles/Home.module.scss'
 import { howWeWorkData, ourServicesData } from './api/data'
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState<Boolean>(true)
+  setTimeout(() => {
+    setIsLoading(false)
+  }, 2000)
   return (
     <>
-      <main className={`${styles.main}`}>
-        <NavBar />
-        <Hero />
-        <HowWeWork
-          title={{
-            mainText: 'OUR',
-            text: 'SERVICES',
-          }}
-          data={ourServicesData}
-        />
-        <OurWork mainText="OUR" text="WORKS" />
-        <HowWeWork
-          title={{
-            mainText: 'HOW WE',
-            text: 'WORK ?',
-          }}
-          data={howWeWorkData}
-        />
-        <Contact />
-        <Footer />
-      </main>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <main className={`${styles.main}`}>
+          <NavBar />
+          <Hero />
+          <HowWeWork
+            title={{
+              mainText: 'OUR',
+              text: 'SERVICES',
+            }}
+            data={ourServicesData}
+          />
+          <OurWork mainText="OUR" text="WORKS" />
+          <HowWeWork
+            title={{
+              mainText: 'HOW WE',
+              text: 'WORK ?',
+            }}
+            data={howWeWorkData}
+          />
+          <Contact />
+          <Footer />
+        </main>
+      )}
     </>
   )
 }
