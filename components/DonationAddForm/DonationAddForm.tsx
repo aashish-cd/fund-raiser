274c3574a41b60b86e1028de5f2d6640c43c6f98
aashish-cd <076bct002@ioepc.edu.np>
2023-06-01 14:07:49 +0545

    button -> start a campaign

--- components/DonationAddForm/DonationAddForm.tsx
+++ components/DonationAddForm/DonationAddForm.tsx
@@ -106,7 +106,7 @@ const DonationAddForm = () => {
                 onSubmit={handleSubmit}
                 className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
               >
-                Button
+                Start a Campaign
               </button>
             </div>
           </form>
